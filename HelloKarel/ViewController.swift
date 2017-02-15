a6098616f4284be9ea83fb4e860a0285fed1b59a
Michael Karr <mkarr1@yahoo.com>
2017-02-14 19:10:48 -0800

    Set up response structure

--- HelloKarel/ViewController.swift
+++ HelloKarel/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var karelText: UITextView!
     @IBOutlet weak var karelImage: UIImageView!
-    @IBOutlet weak var karelText: UILabel!
     @IBOutlet weak var talkField: UITextField!
+    
+    let welcomeMessage = "Talk to Karel!"
+    
+    let responses : [String : String] = [
+        "Hello" : "Hi!",
+        "How are you?" : "Doing great!"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         karelText.text = "Talk to me!"
+        talkField.placeholder = welcomeMessage
+        
+        //talkField.text = welcomeMessage
         //talkField.text = "Talk to Karel!"
+        
+        //class in charge of handling text field
+        talkField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,5 +41,34 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController : UITextFieldDelegate
+{
+    
+    // this would run and replace our given text
+    
+    /*func textFieldDidBeginEditing(textField: UITextField) {
+        //if user taps the text field first time, clear the text field
+        if (textField.text == welcomeMessage) {
+            textField.text = ""
+        }
+
+    }*/
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false;
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        
+        //if let: if assignment is non-null value, perform first block of code
+        if let karelRespone = responses[textField.text!] {
+            karelText.text = karelRespone
+        } else {
+            karelText.text = "No comment"
+        }
+    }
 }
 
